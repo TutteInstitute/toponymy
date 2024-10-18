0fd3ab31444746ac7be9602b8fe664e260e80679
Leland McInnes <leland.mcinnes@gmail.com>
2024-10-18 21:28:56 +0000

    format with black

--- toponymy/llm_wrappers.py
+++ toponymy/llm_wrappers.py
@@ -19,28 +19,23 @@ try:
             self.llm = llama_cpp.Llama(model_path=model_path, **kwargs)
 
         def generate_topic_name(self, prompt, temperature=0.8):
-            topic_name = self.llm(
-                prompt, temperature=temperature)['choices'][0]['text']
+            topic_name = self.llm(prompt, temperature=temperature)["choices"][0]["text"]
             if "\n" in topic_name:
                 topic_name = topic_name.lstrip("\n ")
                 topic_name = topic_name.split("\n")[0]
             topic_name = string.capwords(
-                topic_name.strip(string.punctuation + string.whitespace))
+                topic_name.strip(string.punctuation + string.whitespace)
+            )
             return topic_name
 
-        def generate_topic_cluster_names(self,
-                                         prompt,
-                                         old_names,
-                                         temperature=0.5):
+        def generate_topic_cluster_names(self, prompt, old_names, temperature=0.5):
             try:
                 topic_name_info_raw = self.llm(prompt, temperature=temperature)
-                topic_name_info_text = topic_name_info_raw['choices'][0][
-                    'text']
+                topic_name_info_text = topic_name_info_raw["choices"][0]["text"]
                 topic_name_info = json.loads(topic_name_info_text)
                 result = []
                 for old_name, name_mapping in zip(old_names, topic_name_info):
-                    if old_name.lower() == list(
-                            name_mapping.keys())[0].lower():
+                    if old_name.lower() == list(name_mapping.keys())[0].lower():
                         result.append(list(name_mapping.values()[0]))
                     else:
                         result.append(old_name)
@@ -50,7 +45,7 @@ try:
                 return old_names
 
         def tokenize(self, text):
-            return self.llm.tokenize(text.encode('utf-8'))
+            return self.llm.tokenize(text.encode("utf-8"))
 
         def detokenize(self, tokens):
             return self.llm.detokenize(tokens)
@@ -64,7 +59,7 @@ try:
                 return "\nA better and more specific name that still captures the topic of these article titles is:\n"
             else:
                 raise ValueError(
-                    f"Invalid llm_imnstruction kind; should be one of \'base_layer\', \'intermediate_layer\', or \'remedy\' not \'{kind}\'"
+                    f"Invalid llm_imnstruction kind; should be one of 'base_layer', 'intermediate_layer', or 'remedy' not '{kind}'"
                 )
 
         def n_ctx(self):
@@ -81,10 +76,7 @@ try:
 
     class CohereWrapper:
 
-        def __init__(self,
-                     API_KEY,
-                     model="command-r-08-2024",
-                     local_tokenizer=None):
+        def __init__(self, API_KEY, model="command-r-08-2024", local_tokenizer=None):
             self.llm = cohere.Client(api_key=API_KEY)
 
             try:
@@ -113,10 +105,7 @@ try:
                 topic_name = ""
             return topic_name
 
-        def generate_topic_cluster_names(self,
-                                         prompt,
-                                         old_names,
-                                         temperature=0.5):
+        def generate_topic_cluster_names(self, prompt, old_names, temperature=0.5):
             try:
                 topic_name_info_raw = self.llm.chat(
                     message=prompt,
@@ -126,15 +115,13 @@ try:
                 topic_name_info_text = topic_name_info_raw.text
                 topic_name_info = json.loads(topic_name_info_text)
             except Exception as e:
-                warn(
-                    f"Failed to generate topic cluster names with Cohere: {e}")
+                warn(f"Failed to generate topic cluster names with Cohere: {e}")
                 return old_names
 
             result = []
             for old_name, name_mapping in zip(old_names, topic_name_info):
                 try:
-                    if old_name.lower() == list(
-                            name_mapping.keys())[0].lower():
+                    if old_name.lower() == list(name_mapping.keys())[0].lower():
                         result.append(list(name_mapping.values())[0])
                     else:
                         warn(
@@ -151,8 +138,7 @@ try:
             return self.tokenizer.tokenize(text=text, model=self.model).tokens
 
         def detokenize(self, tokens):
-            return self.tokenizer.detokenize(tokens=tokens,
-                                             model=self.model).text
+            return self.tokenizer.detokenize(tokens=tokens, model=self.model).text
 
         def llm_instruction(self, kind="base_layer"):
             if kind == "base_layer":
@@ -175,11 +161,12 @@ The response should be in JSON formatted as {"topic_name":<NAME>, "less_specific
 """
             else:
                 raise ValueError(
-                    f"Invalid llm_imnstruction kind; should be one of \'base_layer\', \'intermediate_layer\', or \'remedy\' not \'{kind}\'"
+                    f"Invalid llm_imnstruction kind; should be one of 'base_layer', 'intermediate_layer', or 'remedy' not '{kind}'"
                 )
 
         def n_ctx(self):
             return self.llm.models.get(self.model).context_length
+
 except:
     pass
 
@@ -191,10 +178,9 @@ try:
 
     class AnthropicWrapper:
 
-        def __init__(self,
-                     API_KEY,
-                     model="claude-3-haiku-20240307",
-                     local_tokenizer=None):
+        def __init__(
+            self, API_KEY, model="claude-3-haiku-20240307", local_tokenizer=None
+        ):
             self.llm = anthropic.Anthropic(api_key=API_KEY)
             self.model = model
             if local_tokenizer is not None:
@@ -207,11 +193,9 @@ try:
                 topic_name_info_raw = self.llm.messages.create(
                     model=self.model,
                     max_tokens=256,
-                    messages=[{
-                        "role": "user",
-                        "content": prompt
-                    }],
-                    temperature=temperature)
+                    messages=[{"role": "user", "content": prompt}],
+                    temperature=temperature,
+                )
                 topic_name_info_text = topic_name_info_raw.content[0].text
                 topic_name_info = json.loads(topic_name_info_text)
                 topic_name = topic_name_info["topic_name"]
@@ -219,25 +203,19 @@ try:
                 topic_name = ""
             return topic_name
 
-        def generate_topic_cluster_names(self,
-                                         prompt,
-                                         old_names,
-                                         temperature=0.5):
+        def generate_topic_cluster_names(self, prompt, old_names, temperature=0.5):
             try:
                 topic_name_info_raw = self.llm.messages.create(
                     model=self.model,
                     max_tokens=1024,
-                    messages=[{
-                        "role": "user",
-                        "content": prompt
-                    }],
-                    temperature=temperature)
+                    messages=[{"role": "user", "content": prompt}],
+                    temperature=temperature,
+                )
                 topic_name_info_text = topic_name_info_raw.content[0].text
                 topic_name_info = json.loads(topic_name_info_text)
                 result = []
                 for old_name, name_mapping in zip(old_names, topic_name_info):
-                    if old_name.lower() == list(
-                            name_mapping.keys())[0].lower():
+                    if old_name.lower() == list(name_mapping.keys())[0].lower():
                         result.append(list(name_mapping.values()[0]))
                     else:
                         result.append(old_name)
@@ -273,11 +251,12 @@ The response should be only JSON with no preamble formatted as {"topic_name":<NA
 """
             else:
                 raise ValueError(
-                    f"Invalid llm_imnstruction kind; should be one of \'base_layer\', \'intermediate_layer\', or \'remedy\' not \'{kind}\'"
+                    f"Invalid llm_imnstruction kind; should be one of 'base_layer', 'intermediate_layer', or 'remedy' not '{kind}'"
                 )
 
         def n_ctx(self):
             return 128_000
+
 except:
     pass
 
@@ -300,14 +279,11 @@ try:
                 topic_name_info_raw = self.llm.chat.completions.create(
                     model=self.model,
                     max_tokens=256,
-                    messages=[{
-                        "role": "user",
-                        "content": prompt
-                    }],
+                    messages=[{"role": "user", "content": prompt}],
                     temperature=temperature,
-                    response_format={'type': 'json_object'})
-                topic_name_info_text = topic_name_info_raw.choices[
-                    0].message.content
+                    response_format={"type": "json_object"},
+                )
+                topic_name_info_text = topic_name_info_raw.choices[0].message.content
 
                 topic_name_info = json.loads(topic_name_info_text)
                 topic_name = topic_name_info["topic_name"]
@@ -315,30 +291,23 @@ try:
                     print(topic_name_info)
             except Exception as e:
                 topic_name = ""
-                warn(f'{e}\n{prompt}\n{topic_name_info_text}')
+                warn(f"{e}\n{prompt}\n{topic_name_info_text}")
             return topic_name
 
-        def generate_topic_cluster_names(self,
-                                         prompt,
-                                         old_names,
-                                         temperature=0.5):
+        def generate_topic_cluster_names(self, prompt, old_names, temperature=0.5):
             try:
                 topic_name_info_raw = self.llm.chat.completions.create(
                     model=self.model,
                     max_tokens=1024,
-                    messages=[{
-                        "role": "user",
-                        "content": prompt
-                    }],
+                    messages=[{"role": "user", "content": prompt}],
                     temperature=temperature,
-                    response_format={'type': 'json_object'})
-                topic_name_info_text = topic_name_info_raw.choices[
-                    0].message.content
+                    response_format={"type": "json_object"},
+                )
+                topic_name_info_text = topic_name_info_raw.choices[0].message.content
                 topic_name_info = json.loads(topic_name_info_text)
                 result = []
                 for old_name, name_mapping in zip(old_names, topic_name_info):
-                    if old_name.lower() == list(
-                            name_mapping.keys())[0].lower():
+                    if old_name.lower() == list(name_mapping.keys())[0].lower():
                         result.append(list(name_mapping.values()[0]))
                     else:
                         result.append(old_name)
@@ -374,10 +343,11 @@ The response should be only JSON with no preamble formatted as {"topic_name":<NA
 """
             else:
                 raise ValueError(
-                    f"Invalid llm_imnstruction kind; should be one of \'base_layer\', \'intermediate_layer\', or \'remedy\' not \'{kind}\'"
+                    f"Invalid llm_imnstruction kind; should be one of 'base_layer', 'intermediate_layer', or 'remedy' not '{kind}'"
                 )
 
         def n_ctx(self):
             return 128_000
+
 except:
     pass
