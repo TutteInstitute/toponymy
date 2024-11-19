0f2b2176c5e5198bd7609ca4d21d96754bbb1a9a
Leland McInnes <leland.mcinnes@gmail.com>
2024-11-19 18:27:26 +0000

    Add typing information

--- toponymy/clustering.py
+++ toponymy/clustering.py
@@ -9,16 +9,16 @@ from fast_hdbscan.cluster_trees import (
 from fast_hdbscan.boruvka import parallel_boruvka
 from fast_hdbscan.numba_kdtree import kdtree_to_numba
 from scipy.spatial import KDTree
-
+from typing import List, Tuple, Dict, Type, Any
 
 def build_raw_cluster_layers(
-    data,
+    data: np.ndarray,
     *,
-    min_clusters=3,
-    min_samples=5,
-    base_min_cluster_size=10,
-    next_cluster_size_quantile=0.8,
-):
+    min_clusters: int = 3,
+    min_samples: int = 5,
+    base_min_cluster_size: int = 10,
+    next_cluster_size_quantile: float = 0.8,
+) -> List[np.ndarray]:
     n_samples = data.shape[0]
     cluster_layers = []
     min_cluster_size = base_min_cluster_size
@@ -54,10 +54,9 @@ def build_raw_cluster_layers(
 
 
 @numba.njit(cache=True)
-def _build_cluster_tree(labels):
-    mapping = [(-1, -1, -1, -1) for i in range(0)]
-    found = [set([-1]) for i in range(len(labels))]
-    mapping_idx = 0
+def _build_cluster_tree(labels: List[np.ndarray]) -> List[Tuple[int, int, int, int]]:
+    mapping = [(-1, -1, -1, -1) for _ in range(0)]
+    found = [set([-1]) for _ in range(len(labels))]
     for upper_layer in range(1, len(labels)):
         upper_layer_unique_labels = np.unique(labels[upper_layer])
         for lower_layer in range(upper_layer - 1, -1, -1):
@@ -81,7 +80,7 @@ def _build_cluster_tree(labels):
     return mapping
 
 
-def build_cluster_tree(labels):
+def build_cluster_tree(labels: List[np.ndarray]) -> Dict[Tuple[int, int], List[Tuple[int, int]]]:
     result = {}
     raw_mapping = _build_cluster_tree(labels)
     for parent_layer, parent_cluster, child_layer, child_cluster in raw_mapping:
@@ -94,11 +93,11 @@ def build_cluster_tree(labels):
 
 
 @numba.njit()
-def centroids_from_labels(cluster_labels, vector_data):
-    result = np.zeros(cluster_labels.max() + 1, vector_data.shape[1])
+def centroids_from_labels(cluster_labels: np.ndarray, vector_data: np.ndarray) -> np.ndarray:
+    result = np.zeros((cluster_labels.max() + 1, vector_data.shape[1]))
     counts = np.zeros(cluster_labels.max() + 1)
     for i in range(cluster_labels.shape[0]):
-        result[i] += vector_data[cluster_labels[i]]
+        result[cluster_labels[i]] += vector_data[cluster_labels[i]]
         counts[cluster_labels[i]] += 1
 
     for i in range(result.shape[0]):
@@ -108,14 +107,14 @@ def centroids_from_labels(cluster_labels, vector_data):
 
 
 def create_cluster_layers(
-    layer_class,
-    clusterable_vectors,
-    embedding_vectors,
-    min_clusters=6,
-    min_samples=5,
-    base_min_cluster_size=10,
-    next_cluster_size_quantile=0.8,
-):
+    layer_class: Type[Any],
+    clusterable_vectors: np.ndarray,
+    embedding_vectors: np.ndarray,
+    min_clusters: int = 6,
+    min_samples: int = 5,
+    base_min_cluster_size: int = 10,
+    next_cluster_size_quantile: float = 0.8,
+) -> Tuple[List[Any], Dict[Tuple[int, int], List[Tuple[int, int]]]]:
     cluster_labels = build_raw_cluster_layers(
         clusterable_vectors,
         min_clusters=min_clusters,
