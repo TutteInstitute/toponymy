c8881ac40f51b0e59ba98e94cc331dba645af7c1
jc-healy <jchealy@gmail.com>
2024-11-29 22:49:07 +0000

    Improved random exemplar and added centroid based exemplar samplers alogn with some very, very basic unit test.

--- toponymy/exemplar_texts.py
+++ toponymy/exemplar_texts.py
@@ -1,37 +1,142 @@
 import random
 import numpy as np
 from typing import List, Tuple, FrozenSet, Dict, Callable
+from sklearn.metrics import pairwise_distances
+from toponymy.utility_functions import diversify_max_alpha as diversify
+
 
 
 def random_sample_exemplar(
     cluster_label_vector: np.ndarray,
-    object_list: List[str],
-    n_samples: int = 4,
+    objects: List[str],
+    n_exemplars: int = 4,
 ) -> List[List[str]]:
     """Generates a list of exemplar texts for each cluster in a cluster layer.
+    These exemplars are randomly sampled from each cluster.
 
     Parameters
     ----------
     cluster_label_vector : np.ndarray
         A vector of cluster labels for each object.
+    objects : List[str]
+        A list of objects; these are text objects a sample of which are returned as exemplars for each cluster.
     n_samples : int, optional
         The number of exemplars to sample for each cluster, by default 4.
 
     Returns
     -------
-    keyphrases List[List[str]]
-        A list of lists of keyphrases for each cluster.
+    exemplars List[List[str]]
+        A list of lists of exemplar text for each cluster.
     """
 
-    exemplars = []
-    for i in np.unique(cluster_label_vector):
-        indices = np.where(cluster_label_vector==i)[0]
-        if(len(indices)>n_samples):
-            sample_indices = np.random.choice(indices, n_samples, replace=False)
-            sample_paragraphs = object_list[sample_indices] 
-        else:
-            sample_paragraphs = object_list[indices]
-        exemplars.append(sample_paragraphs)
-
-    return exemplars
+    results = []
+    for cluster_num in range(cluster_label_vector.max() + 1):
+        #Grab the vectors associated with the objects in this cluster
+        cluster_objects = np.array(objects)[cluster_label_vector==cluster_num]
+        # Randomly permute the index to create a random selection
+        exemplar_order = np.random.permutation(len(cluster_objects))[:n_exemplars]
+        chosen_exemplars = [cluster_objects[i] for i in exemplar_order]
+        results.append(chosen_exemplars)
+    return results
     
+def centroid_sample_exemplar(
+    cluster_label_vector: np.ndarray,
+    objects: List[str],
+    object_vectors: np.ndarray,
+    centroid_vectors: np.ndarray,
+    n_exemplars: int = 4,
+    diversify_alpha: float = 1.0,
+) -> List[List[str]]:
+    """Generates a list of exemplar text for each cluster in a cluster layer.  
+    These exemplars are selected to be the closest vectors to the cluster centroid while retaining 
+    sufficient diversity.
+
+    Parameters
+    ----------
+    cluster_label_vector : np.ndarray
+        A vector of cluster labels for each object.
+    objects : List[str]
+        A list of objects; these are text objects a sample of which are returned as exemplars for each cluster.
+    object_vectors = np.ndarray
+        An ndarray of topic vectors for each object.
+    centroid_vectors : np.ndarray
+        An ndarray of centroid vectors for each cluster.
+    n_exemplars : int, optional
+        The number of exemplars to sample for each cluster, by default 4.
+    diversify_alpha : float, optional
+        The alpha parameter for diversifying the keyphrase selection, by default 1.0.
+
+    Returns
+    -------
+    exemplars List[List[str]]
+        A list of lists of exemplar text for each cluster.
+    """
+    results = []
+    for cluster_num in range(cluster_label_vector.max() + 1):
+        #Grab the vectors associated with the objects in this cluster
+        cluster_objects = np.array(objects)[cluster_label_vector==cluster_num]
+        cluster_object_vectors = object_vectors[cluster_label_vector==cluster_num]
+
+        # Select the central exemplars as the objects to each centroid
+        exemplar_distances = pairwise_distances(
+            centroid_vectors[cluster_num].reshape(1, -1), cluster_object_vectors, metric="cosine"
+        )
+        exemplar_order = np.argsort(exemplar_distances.flatten())
+        # I'm uncertain about selecting up to n_exemplar**2 candidates
+        exemplar_candidates = [cluster_objects[i] for i in exemplar_order[ : n_exemplars **2]]
+        candidate_vectors = np.asarray([cluster_object_vectors[i] for i in exemplar_order[ : n_exemplars **2]])
+        chosen_indices = diversify(
+            centroid_vectors[cluster_num], candidate_vectors, n_exemplars, max_alpha=diversify_alpha
+        )[:n_exemplars]
+        chosen_exemplars = [exemplar_candidates[i] for i in chosen_indices]
+        results.append(chosen_exemplars)
+    return results
+
+def random_diverse_exemplar(
+    cluster_label_vector: np.ndarray,
+    objects: List[str],
+    object_vectors: np.ndarray,
+    centroid_vectors: np.ndarray,
+    n_exemplars: int = 4,
+    diversify_alpha: float = 1.0,
+) -> List[List[str]]:
+    """Generates a list of exemplar text for each cluster in a cluster layer.  
+    These exemplars are randomly while still requiring sufficient diversity.
+
+    Parameters
+    ----------
+    cluster_label_vector : np.ndarray
+        A vector of cluster labels for each object.
+    objects : List[str]
+        A list of objects; these are text objects a sample of which are returned as exemplars for each cluster.
+    object_vectors = np.ndarray
+        An ndarray of topic vectors for each object.
+    centroid_vectors : np.ndarray
+        An ndarray of centroid vectors for each cluster.
+    n_exemplars : int, optional
+        The number of exemplars to sample for each cluster, by default 4.
+    diversify_alpha : float, optional
+        The alpha parameter for diversifying the keyphrase selection, by default 1.0.
+
+    Returns
+    -------
+    exemplars List[List[str]]
+        A list of lists of exemplar text for each cluster.
+    """
+    results = []
+    for cluster_num in range(cluster_label_vector.max() + 1):
+        #Grab the vectors associated with the objects in this cluster
+        cluster_objects = np.array(objects)[cluster_label_vector==cluster_num]
+        cluster_object_vectors = object_vectors[cluster_label_vector==cluster_num]
+
+        # Randomly permute the index to create a random selection
+        exemplar_order = np.random.permutation(len(cluster_objects))
+        # I'm uncertain about selecting up to n_exemplar**2 candidates
+        exemplar_candidates = [cluster_objects[i] for i in exemplar_order[ : n_exemplars **2]]
+        candidate_vectors = np.asarray([cluster_object_vectors[i] for i in exemplar_order[ : n_exemplars **2]])
+        chosen_indices = diversify(
+            centroid_vectors[cluster_num], candidate_vectors, n_exemplars, max_alpha=diversify_alpha
+        )[:n_exemplars]
+        chosen_exemplars = [exemplar_candidates[i] for i in chosen_indices]
+        results.append(chosen_exemplars)
+    return results
--- toponymy/tests/test_exemplar_texts.py
+++ toponymy/tests/test_exemplar_texts.py
@@ -1,28 +1,72 @@
+from typing import Literal
 import pytest
 import json
 import numpy as np
-from toponymy.exemplar_texts import random_sample_exemplar
+from toponymy.exemplar_texts import random_sample_exemplar, centroid_sample_exemplar, random_diverse_exemplar
 from pathlib import Path
+import sentence_transformers
+
+from toponymy.clustering import (
+    centroids_from_labels,
+)
 
 #import sentence_transformers
-#EMBEDDER = sentence_transformers.SentenceTransformer("all-MiniLM-L6-v2")
+EMBEDDER = sentence_transformers.SentenceTransformer("all-MiniLM-L6-v2")
  
 TEST_OBJECTS = json.load(open(Path(__file__).parent / "test_objects.json", "r"))
 TOPIC_OBJECTS = json.load(open(Path(__file__).parent / "topic_objects.json", "r"))
+ALL_TOPIC_OBJECTS = sum([x["paragraphs"] for x in TOPIC_OBJECTS], [])
+CLUSTER_LAYER = np.concatenate([np.arange(10).repeat(10), np.full(10, -1)])
+TOPIC_VECTORS = EMBEDDER.encode(ALL_TOPIC_OBJECTS)
+CENTROID_VECTORS = centroids_from_labels(CLUSTER_LAYER, TOPIC_VECTORS)
+
 
 def test_json_load():
-    print()
     assert len(TEST_OBJECTS)==125
 
-@pytest.mark.parametrize("n_samples", [4, 15])
-def test_random_exemplar(n_samples):
-    paragraphs = np.asarray(sum([x['paragraphs'] for x in TOPIC_OBJECTS], []))
-    cluster_label_vector = np.concatenate([[i]*len(x['paragraphs']) for i,x in enumerate(TOPIC_OBJECTS)])
-    exemplars = random_sample_exemplar(cluster_label_vector, paragraphs, n_samples=n_samples)
-    assert len(exemplars)==len(TOPIC_OBJECTS)
-    for i,x in enumerate(TOPIC_OBJECTS):
-        #print(f'{len(exemplars[i])} {n_samples} {len(x['paragraphs'])}' )
-        assert len(exemplars[i])==min(n_samples, len(x['paragraphs']))
-
-
+@pytest.mark.parametrize("n_exemplars", [4, 15])
+def test_random_exemplar(n_exemplars):
+    #ALL_TOPIC_OBJECTS -> paragraphs = np.asarray(sum([x['paragraphs'] for x in TOPIC_OBJECTS], []))  
+    #CLUSTER_LAYER -> cluster_label_vector = np.concatenate([[i]*len(x['paragraphs']) for i,x in enumerate(TOPIC_OBJECTS)])  
+    exemplars = random_sample_exemplar(CLUSTER_LAYER, ALL_TOPIC_OBJECTS, n_exemplars=n_exemplars)
+    assert len(exemplars)==CLUSTER_LAYER.max()+1
     
+    for i in range(CLUSTER_LAYER.max()+1):
+        assert len(exemplars[i])==min(n_exemplars, len(TOPIC_OBJECTS[i]['paragraphs']))
+
+
+
+@pytest.mark.parametrize("n_exemplars", [3, 5])
+@pytest.mark.parametrize("diversify_alpha", [0.0, 0.5, 1.0])
+def test_centroid_sample_exemplar_result_sizes(n_exemplars: Literal[3] | Literal[5], diversify_alpha: float):
+    centroid_exemplar_results = centroid_sample_exemplar(
+        cluster_label_vector = CLUSTER_LAYER,
+        objects = ALL_TOPIC_OBJECTS,
+        object_vectors = TOPIC_VECTORS,
+        centroid_vectors = CENTROID_VECTORS,
+        n_exemplars= n_exemplars,
+        diversify_alpha = diversify_alpha,
+    )
+    assert len(centroid_exemplar_results) == len(np.unique(CLUSTER_LAYER)) - 1
+    if diversify_alpha == 1.0:
+        print(centroid_exemplar_results)
+    assert all([len(x) == n_exemplars for x in centroid_exemplar_results]) and centroid_exemplar_results
+    assert all([len(set(x)) == n_exemplars for x in centroid_exemplar_results])
+
+
+@pytest.mark.parametrize("n_exemplars", [3, 5])
+@pytest.mark.parametrize("diversify_alpha", [0.0, 0.5, 1.0])
+def test_random_sample_exemplar_result_sizes(n_exemplars: Literal[3] | Literal[5], diversify_alpha: float):
+    random_exemplar_results = random_diverse_exemplar(
+        cluster_label_vector = CLUSTER_LAYER,
+        objects = ALL_TOPIC_OBJECTS,
+        object_vectors = TOPIC_VECTORS,
+        centroid_vectors = CENTROID_VECTORS,
+        n_exemplars= n_exemplars,
+        diversify_alpha = diversify_alpha,
+    )
+    assert len(random_exemplar_results) == len(np.unique(CLUSTER_LAYER)) - 1
+    if diversify_alpha == 1.0:
+        print(random_exemplar_results)
+    assert all([len(x) == n_exemplars for x in random_exemplar_results]) and random_exemplar_results
+    assert all([len(set(x)) == n_exemplars for x in random_exemplar_results])
