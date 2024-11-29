bebd1e2b083dfc5650278939df7417e5e9d27a10
Leland McInnes <leland.mcinnes@gmail.com>
2024-11-29 18:09:57 +0000

    Fix tests for exmplar texts

--- toponymy/tests/test_exemplar_texts.py
+++ toponymy/tests/test_exemplar_texts.py
@@ -8,7 +8,7 @@ from pathlib import Path
 #EMBEDDER = sentence_transformers.SentenceTransformer("all-MiniLM-L6-v2")
  
 TEST_OBJECTS = json.load(open(Path(__file__).parent / "test_objects.json", "r"))
-#TOPIC_OBJECTS = json.load(open(Path(__file__).parent / "topic_objects.json", "r"))
+TOPIC_OBJECTS = json.load(open(Path(__file__).parent / "topic_objects.json", "r"))
 
 def test_json_load():
     print()
@@ -16,11 +16,11 @@ def test_json_load():
 
 @pytest.mark.parametrize("n_samples", [4, 15])
 def test_random_exemplar(n_samples):
-    paragraphs = np.concatenate([x['paragraphs'] for x in TEST_OBJECTS])
-    cluster_label_vector = np.concatenate([[i]*len(x['paragraphs']) for i,x in enumerate(TEST_OBJECTS)])
+    paragraphs = np.asarray(sum([x['paragraphs'] for x in TOPIC_OBJECTS], []))
+    cluster_label_vector = np.concatenate([[i]*len(x['paragraphs']) for i,x in enumerate(TOPIC_OBJECTS)])
     exemplars = random_sample_exemplar(cluster_label_vector, paragraphs, n_samples=n_samples)
-    assert len(exemplars)==len(TEST_OBJECTS)
-    for i,x in enumerate(TEST_OBJECTS):
+    assert len(exemplars)==len(TOPIC_OBJECTS)
+    for i,x in enumerate(TOPIC_OBJECTS):
         #print(f'{len(exemplars[i])} {n_samples} {len(x['paragraphs'])}' )
         assert len(exemplars[i])==min(n_samples, len(x['paragraphs']))
 
