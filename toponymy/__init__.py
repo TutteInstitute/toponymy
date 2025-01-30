7fcb409a1c2ae590039d515a1ed008ec1986c026
Leland McInnes <leland.mcinnes@gmail.com>
2025-01-30 22:16:58 +0000

    Import relevant classes at the top level

--- toponymy/__init__.py
+++ toponymy/__init__.py
@@ -1,9 +1,11 @@
 from .toponymy import Toponymy
 from .clustering import ToponymyClusterer
 from .keyphrases import KeyphraseBuilder
+from .cluster_layer import ClusterLayerText
 
 __all__ = [
-    "Toponymy"
-    "ToponymyClusterer"
-    "KeyphraseBuilder"
+    "Toponymy",
+    "ToponymyClusterer",
+    "KeyphraseBuilder",
+    "ClusterLayerText",
 ]
