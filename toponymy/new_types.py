d9e92a68fe1a6af6c9b026a27b31b118f62cc4f2
Charles Edward Gagnon <76854355+carloskiki@users.noreply.github.com>
2026-06-22 13:33:02 -0400

    feature type change

--- toponymy/new_types.py
+++ toponymy/new_types.py
@@ -1,9 +1,15 @@
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
 class Cluster:
     members: list[int]
-    features: list
+    features: dict[str, Any]
     prompt: str
     name: str
 
+@dataclass
 class ClusterLayer:
     clusters: list[Cluster]
     layer_index: int
