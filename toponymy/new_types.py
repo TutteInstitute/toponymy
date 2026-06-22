09f8f5c72b8a55d1b2826ab1f7fc5a1d214c3e3c
Ryan DeWolfe <ryandewolfe33@gmail.com>
2026-06-22 14:24:49 -0400

    Define abstrast clusterer interface

--- toponymy/new_types.py
+++ toponymy/new_types.py
@@ -1,9 +1,30 @@
 class Cluster:
-    members: list[int]
-    features: list
+    """
+    Class to store data about a single cluster.
+
+    Attributes
+    ----------
+    members : list[int]
+        A list of data ids that are in this cluster.
+    
+    features: dict[str, Any]
+        Dictionary of features extracted from this cluster.
+
     prompt: str
+        The prompt that will be passed to generate the name.
+
     name: str
+        The name that has been assigned to this cluster.
+    """
+    members: list[int]
+    features: dict[str, Any] | None
+    prompt: str | None
+    name: str | None
+
 
 class ClusterLayer:
+    """
+    Explicit type for a list of all the clusters from a single layer, and the index of that layer.
+    """
     clusters: list[Cluster]
     layer_index: int
